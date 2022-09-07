5df557c14f65925e7209710b788ad32353a637fa
JinWook <ppu2000@inogard.co.kr>
2022-09-07 09:12:28 +0900

    최종본 커밋

--- src/main/java/com/bookPj/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/bookPj/book/springboot/config/auth/SecurityConfig.java
@@ -2,14 +2,12 @@ package com.bookPj.book.springboot.config.auth;
 
 import com.bookPj.book.springboot.domain.user.Role;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
-@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private final CustomOAuth2UserService customOAuth2UserService;
 
